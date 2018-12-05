362a9e45a54aa442a0aa23ea9cdc7d526c3f87c3
hujiao <hujiao@bytedance.com>
2018-12-05 20:36:53 +0800

    添加注释

--- index_react.js
+++ index_react.js
@@ -7,21 +7,27 @@ export default (Target, handleBack) => {
             window.history.go(-1);
         }
         componentDidMount () {
+            // 通过调用 history.pushState() 方法添加一条历史记录
             history.pushState('_android_back', null, location.href);
+            // 监听 popstate 事件，当点击Android物理返回键时，会触发该事件
             window.addEventListener('popstate', this.handlePopstate);
             if (handleBack) {
+                // 添加自定义处理方法
                 this._handles.push(handleBack);
             } else {
+                // 如果没有自定义处理方法，默认调用 window.history.go(-1);
                 this._handles.push(this.back);
             }
         }
         componentWillUnmount () {
             window.removeEventListener('popstate', this.handlePopstate);
         }
+        // 触发一次popstate方法，则调用最新处理方法
         handlePopstate = () => {
             const handle = this._handles.pop();
             handle && handle();
         }
+        // 通过调用push方法，新增一条历史记录，并添加对应处理方法
         push = (state, handle) => {
             if (handle) {
                 history.pushState(state, null, location.href);
